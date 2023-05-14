ee4334c486ab02b16599cb612d9e31e625e231aa
DmitryOntov <Ontoev2016@gmail.com>
2023-05-14 22:35:08 +0800

    Lesson 2. добавил верстку сайта и сделал разбивку на компоненты

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import Categories from "./Components/Categories";
+import Header from "./Components/Header";
+import PizzaBlock from "./Components/PizzaBlock";
+import Sort from "./Components/Sort";
+import "./scss/app.scss";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title={"Мексиканская"} price={500} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
