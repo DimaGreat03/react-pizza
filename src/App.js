4904d499a4e049d66bfeca9e00d0668eaabb4e1e
DmitryOntov <Ontoev2016@gmail.com>
2023-07-02 14:08:32 +0800

    LESSON 10. сделал фильтрацию пиц через бэкенд и настроил пагинацию

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import React from "react";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
-import Cart from "./pages/Cart"
+import Cart from "./pages/Cart";
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState("");
+
   return (
     <div className="wrapper">
-      <Header />
+      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
       <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home searchValue={searchValue} />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,33 @@ import Categories from "../Components/Categories";
 import Sort from "../Components/Sort";
 import Skeleton from "../Components/PizzaBlock/Skeleton";
 import PizzaBlock from "../Components/PizzaBlock";
+import Pagination from "../Components/Pagination/Pagination";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [categoryId, setCategoryId] = React.useState("");
-  const [sortType, setSortType] = React.useState({
-    name: "популярности",
-    sort: "rating",
-  });
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({name: "популярности",sort: "rating",});
   const order = sortType.sort.includes("-") ? "asc" : "desc";
+  const skeletons = [...new Array(6)].map((_, index) => (<Skeleton key={index} />));
   const sortBy = sortType.sort.replace("-", "");
   const category = categoryId > 0 ? `category=${categoryId}` : "";
+  const search = searchValue ? `&search=${searchValue}` : "";
+  const [currentPage, setCurrentPage] = React.useState(1)
+
+  const pizzas = items
+    // .filter((obj) => {
+    //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+    //     return true;
+    //   }
+    //   return false;
+    // })
+    .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
   React.useEffect(() => {
     setIsLoading(true);
     fetch(
-      `https://649299ad428c3d2035d05219.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://649299ad428c3d2035d05219.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((response) => response.json())
       .then((arr) => {
@@ -27,7 +37,7 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scroll(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   return (
     <div className="container">
@@ -39,11 +49,8 @@ const Home = () => {
         <Sort value={sortType} onChangeSort={(obj) => setSortType(obj)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={number => setCurrentPage(number)}/>
     </div>
   );
 };
